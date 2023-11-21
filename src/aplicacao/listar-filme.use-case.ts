1a82269ecb9f3541aaba53124ea4d65426a562d4
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-11-21 09:08:33 -0400

    commit 21/11

--- src/aplicacao/listar-filme.use-case.ts
+++ src/aplicacao/listar-filme.use-case.ts
@@ -1,14 +1,12 @@
-import BancoEmMemoria from "../infra/banco/banco-em-memoria";
 import FilmeRepositorioInterface from "./filme-repositorio-interface";
-export default class ListarFilme{
-constructor(private filmeRepositorio:FilmeRepositorioInterface){}
-public async execute():Promise<Filme[]>{
-    return this.filmeRepositorio.listar()
-}
-}
-type Filme = {
-    id:number,
-    titulo:string,
-    descricao:string,
-    foto:string
+
+export default class ListaFilme{
+    //Construtor
+    constructor(
+        readonly filmeRepositorio:FilmeRepositorioInterface
+    ){}
+    //executar
+    public async executar(){
+        return await this.filmeRepositorio.listar()
+    }
 }
